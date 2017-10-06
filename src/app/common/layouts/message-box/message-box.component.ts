ef0713296407ffa4ceae4f245c785645dba238df
mvm1978 <mvm031978@gmail.com>
2017-10-06 10:23:29 -0400

    processing logged user data received as an object; moving eventEmitter to each corresponding component

---
+++ src/app/common/layouts/message-box/message-box.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'footer',
+    templateUrl: './app/common/layouts/footer/footer.component.html',
+    styleUrls: [
+        './app/common/layouts/footer/footer.component.css'
+    ]
+})
+
+export class FooterComponent {}
